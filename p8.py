7b575e1e5c977755a2a3354f5b2d59e94fdb9535
Swahari-Sasi-Chandana <125136391+Swahari-Sasi-Chandana@users.noreply.github.com>
2023-10-11 20:07:11 +0530

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,5 @@
+a=int(input("Enter value of A :"))
+if(a%2==0):
+    print("A is Even.")
+else:
+    print("A is Odd.")
---
+++ p9.py
@@ -0,0 +1,10 @@
+s_no=int(input("Enter Student Number:"))
+s_name=input("Enter Student Name:")
+s_group=input("Enter Student Group:")
+s1=int(input("Enter marks of Maths:"))
+s2=int(input("Enter marks of Physics:"))
+s3=int(input("Enter marks of Computers:"))
+if(s1>=40 and s2>=40 and s3>=40):
+    print("Pass")
+else:
+    print("Fail")
