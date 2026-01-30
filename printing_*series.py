2d53a03eafc52c914305fce4f338691bcb5928f0
Swahari-Sasi-Chandana <125136391+Swahari-Sasi-Chandana@users.noreply.github.com>
2026-01-30 21:37:02 +0530

    Implement star pattern printing in printing_series.py
    Add a script to print various star patterns.

---
+++ printing_*series.py
@@ -0,0 +1,34 @@
+for i in range(1,4):
+    for j in range(1,4):
+        print("*",end="  ")
+    print()
+
+
+    * * * 
+    * * * 
+    * * * 
+
+
+for i in range(5):
+    for j in range(4):
+        print("*",end=" ")
+    print()
+
+
+   * * * *
+   * * * *
+   * * * *
+   * * * *
+   * * * *
+
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end="  ")
+    print()
+
+
+    * * *
+    * *
+    *
