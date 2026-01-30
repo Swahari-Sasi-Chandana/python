537b9b6d2c6d6e01619ee007dbbce4d8582c41df
Swahari-Sasi-Chandana <125136391+Swahari-Sasi-Chandana@users.noreply.github.com>
2026-01-30 21:13:16 +0530

    Add program to compute minimum sum from two lists
    Implement a program to find the minimum sum from two lists by summing corresponding elements.

---
+++ min_sum_twolists.py
@@ -0,0 +1,9 @@
+#program to find minimum sum from two lists
+n=int(input())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+res=[]
+for i in range(0,n):
+  c=a[i]+b[i]
+  res.append(c)
+print(min(res))
